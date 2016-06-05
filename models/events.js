50e199ad01cae6e193ece9528a136b66d1eb8a81
Jordan Braginton <jordanb@HQ.local>
2016-06-04 19:03:20 -0600

    Quickly updated event model

--- models/events.js
+++ models/events.js
@@ -8,5 +8,11 @@ var EventSchema = new Schema({
 	duration:{type:Number},
 	end:{type:Number},
 	comments:{type: String},
-	bishopricMember:{type:String}
+	bishopricMember:{type:String},
+	backgroundColor: {type:String},
+	borderColor: {type:String},
+	textColor: {type:String},
+	scheduled: {type: Boolean}
 });
+//some of these can be virtual functions
+//bishopric member and event type are big components in determinging background, border, and text color
